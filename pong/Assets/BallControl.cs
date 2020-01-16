066d507e6b5d18ec69ec2a24989933e9ff66ec85
Pronoy Chopra <pronoyc@gmail.com>
2020-01-16 06:26:47 -0800

    Added resetball function

--- pong/Assets/BallControl.cs
+++ pong/Assets/BallControl.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour {
 
-	private RigidBody2D rb2d;
+	private Rigidbody2D rb2d;
 	private Vector2 vel;
 
 	// Use this for initialization
 	void Start () {
-		rb2d = GetComponent<RigidBody2D>();
+		rb2d = GetComponent<Rigidbody2D>();
 		Invoke("GoBall", 2);
 	}
 	
@@ -24,6 +24,20 @@ public class BallControl : MonoBehaviour {
 			rb2d.AddForce(new Vector2(20, -15));
 		} else {
 			rb2d.AddForce(new Vector2(-20, 15));
+		}
+	}
+
+	void ResetBall(){
+    	rb2d.velocity = Vector2.zero;
+    	transform.position = Vector2.zero;
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		if(coll.collider.CompareTag("Player")){
+			Vector2 vel;
+			vel.x = rb2d.velocity.x;
+			vel.y = (rb2d.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
+			rb2d.velocity = vel;
 		}
 	}
 }
