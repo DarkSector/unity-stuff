bc70338d098de241e09186616ab1a915e627ce7d
Pronoy Chopra <pronoyc@gmail.com>
2020-01-30 00:03:56 -0800

    Removed debug

--- snek/Assets/ShowBorders.cs
+++ snek/Assets/ShowBorders.cs
@@ -14,24 +14,16 @@ public class ShowBorders : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start called");
         float height = 2f * m_OrthographicCamera.orthographicSize;
         float width = height * m_OrthographicCamera.aspect;
 
-        Debug.Log("width is " + width);
-
         float leftBorderPosition = -1f * (width / 2) + 0.5f; 
         float rightBorderPosition = width / 2 - 0.5f;
-
-        Debug.Log("leftBorder: " + leftBorderPosition);
-        Debug.Log("Right border:  " + rightBorderPosition);
 
         BorderLeft.transform.position = new Vector3(leftBorderPosition, 0, 0);
         BorderRight.transform.position = new Vector3(rightBorderPosition, 0, 0);
         float spriteWidth = BorderTop.GetComponent<SpriteRenderer>().bounds.size.x;
         float ratio = width / spriteWidth;
-
-        Debug.Log("Scale Ratio: " + ratio);
 
         BorderTop.transform.localScale = new Vector3(ratio, 1, 1);
         BorderBottom.transform.localScale = new Vector3(ratio, 1, 1);
