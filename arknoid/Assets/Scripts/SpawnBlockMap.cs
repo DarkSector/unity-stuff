09b4e00e4bda57db2c5aa00b405f7860406bc060
Pronoy Chopra <pronoyc@gmail.com>
2020-02-05 23:35:53 -0800

    Spawn blocks for the ball to hit

---
+++ arknoid/Assets/Scripts/SpawnBlockMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This particular script is responsible for spawning multiple Block.Prefabs
+// It will position them according to the given pattern
+// Currently the pattern is just a bunch of them in a row, created manually
+// maybe later we'll be able to place them according to curves or mathematical figures
+// that would be cool
+public class SpawnBlockMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
