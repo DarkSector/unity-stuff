8596c3f07b35e4436236ffb24a85bc3495372395
Pronoy Chopra <pronoyc@gmail.com>
2020-02-05 23:31:12 -0800

    Moving player and added x world bounds

--- arknoid/Assets/Scripts/PlayerControl.cs
+++ arknoid/Assets/Scripts/PlayerControl.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
-{
+{   
+    public Rigidbody2D rb2d;
+    public float speed = 20.0f;
+    KeyCode moveLeft = KeyCode.LeftArrow;
+    KeyCode moveRight = KeyCode.RightArrow;
     // Start is called before the first frame update
+    public Camera m_OrthographicCamera;
+    public float leftBorderPosition;
+    public float rightBorderPosition;
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        float height = 2f * m_OrthographicCamera.orthographicSize;
+        float width = height * m_OrthographicCamera.aspect;
+
+        float leftBorderPosition = -1f * (width / 2) + 0.5f; 
+        float rightBorderPosition = width / 2 - 0.5f;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        var pos = rb2d.position;
+        if (Input.GetKey(moveLeft))
+		{
+			pos.x -= 0.5f;
+		}
+		else if (Input.GetKey(moveRight))
+
+		{
+			pos.x += 0.5f;
+		}
+
+        rb2d.position = pos;
+
     }
+
+
 }
